b938b396b6e7390a1fee59c5d75e16e94fe6dff4
Xeno333 <Xeno333@mailfence.com>
2025-04-06 23:50:42 -0500

    crash fixes and start of inv

--- classes.py
+++ classes.py
@@ -5,12 +5,9 @@ from vars_and_stuff import *
 class Sprite(pygame.sprite.Sprite):
 	def __init__(self, x, y, l, h, png):
 		super().__init__()
-		self.image = pygame.Surface((l, h))
 		self.image = pygame.image.load(png)
 		self.rect = self.image.get_rect()
 		self.rect.center = (x, y)
-		self.alive = True
-		self.direction = 1
 
 # class for other sprites
 class simplesprite(pygame.sprite.Sprite):
