20924ea43839fc60e64891281f5cad2bb9d563b2
Xeno333 <Xeno333@mailfence.com>
2025-04-08 00:24:15 -0500

    updates

--- classes.py
+++ classes.py
@@ -69,10 +69,10 @@ class Inventory:
 				self.value[i].combine(itemstack)
 				return True
 
-			elif self.value[i].same_as(No_Item):
+			elif self.value[i].same_as(No_Item) and free == -1:
 				free = i
 
-		if free >= 0:
+		if free != -1:
 			self.value[i] = itemstack
 			return True
 
